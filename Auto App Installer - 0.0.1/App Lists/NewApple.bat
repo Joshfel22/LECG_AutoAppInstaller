ac101b401549c76b063ea1c6bcf0a86636d74ec7
lachydaves25 <37570905+lachydaves25@users.noreply.github.com>
2023-02-23 14:08:38 +1100

    Delete Auto App Installer - 0.0.1 directory

--- Auto App Installer - 0.0.1/App Lists/NewApple.bat
+++
@@ -1,7 +0,0 @@
-winget uninstall Apple.iTunes --accept-package-agreements --accept-source-agreements -s msstore
-winget uninstall 9PB2MZ1ZMB1S --accept-package-agreements --accept-source-agreements -s msstore
-winget install 9PFHDD62MXS1 --accept-package-agreements --accept-source-agreements -s msstore
-winget install 9PFHDD62MXS1 --accept-package-agreements --accept-source-agreements -s msstore
-winget install 9NM4T8B9JQZ1 --accept-package-agreements --accept-source-agreements -s msstore
-winget install 9NP83LWLPZ9K --accept-package-agreements --accept-source-agreements -s msstore
-pause
