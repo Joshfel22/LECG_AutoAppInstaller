782a069c765dd970acfc9b2601a38f12db01575a
lachydaves25 <37570905+lachydaves25@users.noreply.github.com>
2023-02-23 14:08:29 +1100

    Delete Auto App Installer - 0.0.2 directory

--- Auto App Installer - 0.0.2/AVs/AV - Webroot.bat
+++
@@ -1,5 +0,0 @@
-winget uninstall {4493F3B0-51DA-11EC-8AA4-3863BB3CB5A8}
-winget uninstall {211261B3-EDC2-4125-8139-AE72CFCDF7C8}
-winget uninstall {6F14D6F0-7663-11ED-9748-10604B96B11C}
-winget install Webroot.SecureAnywhere --accept-package-agreements --accept-source-agreements -s winget
-pause
--- Auto App Installer - 0.0.2/App Lists/Home.bat
+++
@@ -1,6 +0,0 @@
-winget install Google.Chrome --accept-package-agreements --accept-source-agreements -s winget 
-winget install XPDM1ZW6815MQM --accept-package-agreements --accept-source-agreements -s msstore
-winget install XPDM17HK323C4X --accept-package-agreements --accept-source-agreements -s msstore
-winget install 9PB2MZ1ZMB1S --accept-package-agreements --accept-source-agreements -s msstore
-winget install XPDP273C0XHQH2 --accept-package-agreements --accept-source-agreements -s msstore
-pause
