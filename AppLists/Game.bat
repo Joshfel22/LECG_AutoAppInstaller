808980b7fd82c0f8d945e24de664d4dea598ce02
Lachlan Davies <37570905+lachydavies25@users.noreply.github.com>
2024-02-15 15:47:13 +1100

    Add files via upload
    We uploaded version of AppLists

---
+++ AppLists/Game.bat
@@ -0,0 +1,5 @@
+winget install GOG.Galaxy --accept-package-agreements --accept-source-agreements -s winget 
+winget install Valve.Steam --accept-package-agreements --accept-source-agreements -s winget 
+winget install XP99VR1BPSBQJ2--accept-package-agreements --accept-source-agreements -s msstore
+winget install XPDC2RH70K22MN--accept-package-agreements --accept-source-agreements -s msstore
+pause
