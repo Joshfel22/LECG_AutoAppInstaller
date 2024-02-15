2d8ba610b338b3c979b32a5510017ced788852e0
Lachlan Davies <37570905+lachydavies25@users.noreply.github.com>
2024-02-15 15:25:36 +1100

    Newest version has removed AV uninstaller code

--- AutoAppsInstaller.bat
+++ AutoAppsInstaller.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Please type or paste the file directory of where I am located.
 echo This is important as command prompt can't figure out if you're running it as a standard user or as administrator.
-echo Because technically speaking, command prompt administrator is a different programme.
+echo Because technically speaking, command prompt administrator uses different variables.
 echo Therefore, I cannot set up a path that predicts where I am located.
 echo ...
 echo where I am located (Case Sensitive)
@@ -11,24 +11,6 @@ echo Please type the name of the app list you would like to use (It is case sens
 set /p alist= 
 echo ...
 call "%where%\App Lists\%alist%.bat"
-echo ...
-echo Do you need AV?
-echo ...
-echo 1 - Webroot
-echo 2 - Trend
-echo 3 - No
-echo ...
-SET /P M=Type 1, 2, or 3 then press ENTER:
-IF %M%==1 GOTO WEBROOT
-IF %M%==2 GOTO TREND
-IF %M%==3 GOTO UPDATE
-echo Invalid input, skipping to updates...
-GOTO UPDATE
-:WEBROOT:
-call "%where%\AVs\Webroot.bat"
-:TREND:
-call "%where%\AVs\Trend.bat"
-:UPDATE:
 echo ...
 echo Do you want to update these Apps?
 echo ...
